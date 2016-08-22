3800594bd14499bbe81351cb62b57dbf861a2f46
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-08-22 15:23:17 +0100

    Register admin models

--- compass/admin.py
+++ compass/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book, Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+admin.site.register(Book)
+admin.site.register(Category, CategoryAdmin)
