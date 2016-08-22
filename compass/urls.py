5158d17657218fe8ab7b4b80c8c83794555bfaf1
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-08-22 15:25:32 +0100

    Restructure routes for scale

--- compass/urls.py
+++ compass/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/$', views.index, name='index'),
-    url(r'^/categories$', views.categories, name='category:master'),
-    url(r'^/categories/(?P<category_slug>\w+)$',
-        views.category, name='category:detail'),
-    url(r'^/categories/(?P<category_slug>\w+)/books/(?P<book_slug>\w+)$',
-        views.book, name='book:detail'),
-    url(r'^/search$',
-        views.search, name='compass:find')
+    url(r'^$', views.index, name='index'),
+    url(r'^categories$', views.categories, name='category_master'),
+    url(r'^categories/(?P<category_slug>[\w-]+)$',
+        views.category, name='category_detail'),
+    url(r'^books/(?P<book_slug>[\w-]+)$',
+        views.book, name='book_detail'),
+    url(r'^search$',
+        views.search, name='compass_find')
 ]
