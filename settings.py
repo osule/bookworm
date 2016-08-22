f6f4cce436d15a40d21f9f38adc4b73bdfab35fa
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-08-22 15:38:16 +0100

    Use database url set in heroku

--- settings.py
+++ settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,12 +76,14 @@ WSGI_APPLICATION = 'bookworm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASES = {}
+if not os.getenv('DATABASE_URL'):
+    DATABASES['default'] = {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+else:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Internationalization
