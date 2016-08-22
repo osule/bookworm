185877f1f24cbb3f91af6ec434272fd7ab31787f
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-08-22 13:24:14 +0100

    Stub out views

---
+++ compass/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def index(request,):
+    pass
+
+
+def categories(request, ):
+    pass
+
+
+def category(request, category_slug):
+    pass
+
+
+def book(request, category_slug, book_slug):
+    pass
+
+
+def search(request):
+    pass
