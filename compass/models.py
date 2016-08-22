924132aa28f98ac67350691ad5fac1a6572901a8
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-08-22 14:19:47 +0100

    generate result heading

--- compass/models.py
+++ compass/models.py
@@ -8,6 +8,10 @@ class Category(models.Model):
     slug = models.CharField(max_length=255)
 
     @classmethod
+    def all(cls):
+        return cls.objects.all()
+
+    @classmethod
     def create(cls, title):
         now = datetime.now()
         return cls.objects.create(title=title,
@@ -24,6 +28,10 @@ class Book(models.Model):
     category = models.ForeignKey(Category)
 
     @classmethod
+    def all(cls):
+        return cls.objects.all()
+
+    @classmethod
     def create(cls, title, category):
         now = datetime.now()
         return cls.objects.create(title=title,
@@ -37,5 +45,23 @@ class Book(models.Model):
 
 class Compass(object):
     @classmethod
-    def delegate(cls,):
-        pass
+    def find(cls, **kwargs):
+        title = kwargs.get('title')
+        category = kwargs.get('category')
+        if not title and not category:
+            return Book.all()
+        if not title:
+            return Book.find(category=Category.find(title=category))
+        if not category:
+            return Book.find(title=title)
+
+    @classmethod
+    def heading(cls, **kwargs):
+        title = kwargs.get('title')
+        category = kwargs.get('category')
+        if not title and not category:
+            return "All books"
+        if not title:
+            return "All book titles under {}".format(category)
+        if not category:
+            return "All book titles like {}".format(title)
