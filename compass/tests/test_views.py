0545e665167e01bf3d3f8484280b8be21cca0e77
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-08-22 15:25:06 +0100

    Assert that books & categories can viewed alike

--- compass/tests/test_views.py
+++ compass/tests/test_views.py
@@ -2,7 +2,6 @@ from django.test import TestCase, Client
 from ..models import Category, Book
 
 
-
 class CompassTest(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -11,38 +10,36 @@ class CompassTest(TestCase):
 
     def test_can_view_search_page(self):
         response = self.client.get('/')
-        self.assertContains(response, '<input name="title" type="text"/>')
+        self.assertContains(response, '<input name="title" type="text">')
 
     def test_can_view_categories_page(self):
-        response = self.client.get('/')
-        self.assertContains(response, '<title>Book Categories</title>')
-
-    def test_can_view_books_page(self):
-        response = self.client.get('/categories/')
-        self.assertContains(response, '<title>Book Categories</title>')
+        response = self.client.get('/categories')
+        self.assertContains(response, 'Categories')
 
     def test_can_view_category_page(self):
-        Category.create(title="Mock Category")
-        response = self.client.get('/categories/mock-category-2016-08-22')
-        self.assertContains(response, '<title>Mock Category</title>')
+        Category.create(title="Category 1")
+        response = self.client.get('/categories/category-1-2016-08-22')
+        self.assertContains(response, 'Category 1')
 
     def test_can_view_book_page(self):
         category = Category.create(title="Mock Category")
-        Book.create(title="Mock book", category=category)
+        Book.create(title="Book 1", category=category)
         response = self.client.get(
-            '/categories/mock-category-2016-08-22/books/mock-book-2016-08-22')
-        self.assertContains(response, '<title>Mock Book</title>')
+            '/books/book-1-2016-08-22')
+        self.assertContains(response, 'Book 1')
 
     def test_can_search_book_using_category_and_title(self):
         category = Category.create(title="Mock Category")
-        Book.create(title="Mock book", category=category)
+        Book.create(title="Book 1", category=category)
 
         response = self.client.post('/search', {
-                "title": "Mock book",
+                "title": "Book 1",
                 "category": "Mock category",
             })
 
-        self.assertContains(response, 'Mock book')
+        self.assertContains(
+            response,
+            'All books like Book 1 under Mock category')
 
     def test_can_search_book_using_only_title(self):
         category = Category.create(title="Mock Category")
@@ -51,7 +48,7 @@ class CompassTest(TestCase):
                 "title": "Mock book",
                 "category": "",
             })
-        self.assertContains(response, 'Mock book')
+        self.assertContains(response, 'All book titles like Mock book')
 
     def test_can_search_using_only_category(self):
         category = Category.create(title="Mock Category")
@@ -60,4 +57,4 @@ class CompassTest(TestCase):
                 "title": "",
                 "category": "Mock Category",
             })
-        self.assertContains(response, 'Mock book')
+        self.assertContains(response, 'All book titles under Mock Category')
